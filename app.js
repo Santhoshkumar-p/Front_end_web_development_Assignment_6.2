6f858ffee41b1714e9455c2c3718d1e8524c61dd
Santhoshkumar-p <sanssoft12@gmail.com>
2017-01-17 21:35:22 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+var a=10,b=100,c=1000;
+if (a>b  && a>c)
+{
+console.log("A is greater of all");
+}
+else if (b>a  && b>c)
+{
+console.log("B is greater of all");
+}
+else
+{
+console.log("C is greater of all");
+}
